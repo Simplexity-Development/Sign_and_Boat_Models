70380ab66ccfa13d18d85776ed78554c4eb3b9c5
RhythmicSys <mc.adhdmc@gmail.com>
2023-08-13 09:23:46 -0700

    debug stuff

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-calculate_hashes() {
-    file_path=$1
-    sha256_hash=$(sha256sum "$file_path" | awk '{ print $1 }')
-    sha512_hash=$(sha512sum "$file_path" | awk '{ print $1 }')
-    echo "$sha256_hash" "$sha512_hash"
-}
+#calculate_hashes() {
+#    file_path=$1
+#    sha256_hash=$(sha256sum "$file_path" | awk '{ print $1 }')
+#    sha512_hash=$(sha512sum "$file_path" | awk '{ print $1 }')
+#    echo "$sha256_hash" "$sha512_hash"
+#}
 # Function to compare versions
-compare_versions() {
-    version1=$1
-    version2=$2
-    if [[ $version1 == "$version2" ]]; then
-        return 0  # Versions are equal
-    elif [[ $version1 > $version2 ]]; then
-        return 1  # version1 is greater
-    else
-        return 2  # version2 is greater
-    fi
-}
+#compare_versions() {
+#    version1=$1
+#    version2=$2
+#    if [[ $version1 == "$version2" ]]; then
+#        return 0  # Versions are equal
+#    elif [[ $version1 > $version2 ]]; then
+#        return 1  # version1 is greater
+#    else
+#        return 2  # version2 is greater
+#    fi
+#}
 
 # Read JSON file
 packs_json="packs.json"  # Adjust the JSON file path as needed
@@ -40,12 +40,14 @@ echo "RAW JSON DATA:"
 echo "$data"
 
 # Iterate over packs
-for pack in $(echo "$data" | jq -c '.packs[]'); do
+for pack in $(echo "$data" | jq -r '.packs | keys[]'); do
     echo "STARTING ITERATION OVER PACK - PACK VARIABLES:"
-    name=$(echo "$pack" | jq -r 'keys[0]')
-    echo "PACK VARIABLES INFO: 'name' = $name"
-    pack_data=$(echo "$pack" | jq -r ".$name")
+    index_number="$pack"
+    echo "PACK VARIABLES INFO: 'index_number' = $index_number"
+    pack_data=$(echo "$data" | jq -r "$pack.$index_number")
     echo "PACK VARIABLES INFO: 'pack_data' = $pack_data"
+    name=$(echo "pack_data" | jq -r '.name')
+    echo "PACK VARIABLES INFO: 'name' = $name"
     version=$(echo "$pack_data" | jq -r '.version')
     echo "PACK VARIABLES INFO: 'version' = $version"
     changelog=$(echo "$pack_data" | jq -r '.changelog')
@@ -60,46 +62,46 @@ for pack in $(echo "$data" | jq -c '.packs[]'); do
      # Create zip file
     zip_filename="${name}_${version}.zip"
     echo "ZIP INFO: 'zip_filename' = $zip_filename"
-    pushd "$name" || exit  # Move into the directory
-    zip -r "../$zip_filename" ./*  # Include only the contents of the directory
-    popd || exit  # Move back to the original directory
+    #pushd "$name" || exit  # Move into the directory
+    #zip -r "../$zip_filename" ./*  # Include only the contents of the directory
+    #popd || exit  # Move back to the original directory
 
     # Calculate size of the zip file
-    zip_size=$(du -b "$zip_filename" | awk '{ print $1 }')
-    echo "ZIP INFO: 'zip_size' = $zip_size"
+    #zip_size=$(du -b "$zip_filename" | awk '{ print $1 }')
+    #echo "ZIP INFO: 'zip_size' = $zip_size"
     # Create zip file
-    zip -r "$zip_filename" "$name"
+    #zip -r "$zip_filename" "$name"
 
     # Calculate hashes for the zip file
-    hashes=("$(calculate_hashes "$zip_filename")")
-    sha256_hash=${hashes[0]}
-    sha512_hash=${hashes[1]}
+    #hashes=("$(calculate_hashes "$zip_filename")")
+    #sha256_hash=${hashes[0]}
+    #sha512_hash=${hashes[1]}
 
     # Rest of the deployment logic...
 
     # Construct the data for the POST request
-    post_data=$(jq -n \
-            --arg name "$name" \
-            --arg version_number "$version" \
-            --argjson dependencies "$dependencies" \
-            --argjson game_versions "$minecraft_versions" \
-            --arg version_type "$type" \
-            --argjson changelog "$changelog" \
-            --argjson loaders "$loaders" \
-            --argjson featured "$featured" \
-            --arg url "https://cdn.modrinth.com/data/$modrinth_id/versions/$version/$zip_filename" \
-            --arg filename "$zip_filename" \
-            --arg primary false \
-            --arg size "$zip_size" \
-            --arg sha256 "$sha256_hash" \
-            --arg sha512 "$sha512_hash" \
-            '{name: $name, version_number: $version_number, dependencies: $dependencies, game_versions: $game_versions, version_type: $version_type, changelog: $changelog, loaders: $loaders, featured: $featured, files: [{"hashes": {"sha256": $sha256, "sha512": $sha512}, "url": $url, "filename": $filename, "primary": $primary, "size": ($size | tonumber), "file_type": "required-resource-pack"}]}')
+    #post_data=$(jq -n \
+    #        --arg name "$name" \
+    #        --arg version_number "$version" \
+    #        --argjson dependencies "$dependencies" \
+    #        --argjson game_versions "$minecraft_versions" \
+    #        --arg version_type "$type" \
+    #        --argjson changelog "$changelog" \
+    #        --argjson loaders "$loaders" \
+    #        --argjson featured "$featured" \
+    #        --arg url "https://cdn.modrinth.com/data/$modrinth_id/versions/$version/$zip_filename" \
+    #        --arg filename "$zip_filename" \
+    #        --arg primary false \
+    #        --arg size "$zip_size" \
+    #        --arg sha256 "$sha256_hash" \
+    #        --arg sha512 "$sha512_hash" \
+    #        '{name: $name, version_number: $version_number, dependencies: $dependencies, game_versions: $game_versions, version_type: $version_type, changelog: $changelog, loaders: $loaders, featured: $featured, files: [{"hashes": {"sha256": $sha256, "sha512": $sha512}, "url": $url, "filename": $filename, "primary": $primary, "size": ($size | tonumber), "file_type": "required-resource-pack"}]}')
 
     # Send the POST request using curl
-    curl -X POST "https://api.modrinth.com/v2/project/$modrinth_id/version" \
-        -H "Authorization: Bearer $MODRINTH_TOKEN" \
-        -H "Content-Type: application/json" \
-        -d "$post_data"
+    #curl -X POST "https://api.modrinth.com/v2/project/$modrinth_id/version" \
+    #    -H "Authorization: Bearer $MODRINTH_TOKEN" \
+    #    -H "Content-Type: application/json" \
+    #    -d "$post_data"
 
     if [ $? -eq 0 ]; then
         echo "Version creation successful for $name $version"
